a667f4e3621de648311a9a0c879b9edc1a855ee1
Konrad Banaszek <KOB@kmd.dk>
2019-04-05 17:28:15 +0200

    Project init.

---
+++ src/Kmd.Logic.Cpr.ConsoleSample/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kmd.Logic.Cpr.ConsoleSample
+{
+    internal class AppConfiguration
+    {
+        public LogicEnvironmentConfiguration[] LogicEnvironments { get; set; }
+        public string LogicEnvironmentName { get; set; }
+        public LogicAccountConfiguration LogicAccount { get; set; }
+    }
+
+    internal class LogicEnvironmentConfiguration
+    {
+        public string Name { get; set; }
+        public Uri AuthorizationServerTokenIssuerUri { get; set; }
+        public Uri ScopeUri { get; set; }
+        public Uri ApiRootUri { get; set; }
+    }
+
+    internal class LogicAccountConfiguration
+    {
+        public Guid? SubscriptionId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ src/Kmd.Logic.Cpr.ConsoleSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Kmd.Logic.Cpr.ConsoleSample
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            InitLogger();
+
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(args)
+                    .Build()
+                    .Get<AppConfiguration>();
+
+                await Run(config);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Caught a fatal unhandled exception");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void InitLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        private static async Task Run(AppConfiguration config)
+        {
+
+        }
+    }
+}
